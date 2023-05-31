0d18d29d29bf016e843640b66f7833be158702ec
Milos Grabovcic <48414851+grakseasy@users.noreply.github.com>
2023-05-31 11:33:17 +0200

    Add files via upload

--- skripta.py
+++ skripta.py
@@ -24,15 +24,19 @@ for signal in column_data:
 coverpoints = list(zip(signals, column_data))
 
 with open('ahb_coverage.sv', 'w') as file:
+    file.write('`define cov\n')
+    file.write('`ifdef COVERAGE_EN\n\n')
+    file.write('covergroup ' + covergroup_name + ' with function sample(sequence_item item);\n')
     file.write('covergroup ' + covergroup_name + ';\n')
     file.write('option.per_instance = ' + str(1) + ';\n')
     file.write('option.name = "' + covergroup_name + '";\n\n')
     for cp_name, cp_bins in coverpoints:
-        file.write('    coverpoint_' + cp_name + '      : coverpoint ' + cp_name + '\n')
+        file.write('    coverpoint_' + cp_name + '      : coverpoint item.' + cp_name + '\n')
         file.write('    {\n')
         for i, bin_range in enumerate(cp_bins.split('{')[1].split('}')[0].split(',')):
             file.write('        bins    ' + cp_name + '_' + str(i) + ' = {' + bin_range.strip() + '};\n')
         file.write('    }\n')
     file.write('endgroup\n')
+    file.write('\n\n`endif')
 
 #coverpoints list: ('HRDATA', 'SPLIT_BINS:HRDATA= { [32'h0:32'h66666665], 32'h66666666:32'hcccccccb, 32'hcccccccc:32'hffffffff}'), ...
